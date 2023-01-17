51f553eb2c20e0a303924cd2c0d26a4a5af1db0d
boubalourson <boobaloursonZEBI@gmail.com>
2023-01-17 08:29:08 +0100

    Test Fetch (fonctionne pas :/)

--- Main/script.js
+++ Main/script.js
@@ -1,7 +1,16 @@
-fetch('Data/Movies.json')
-    .then(response => response.json())
-    .then(response2 => console.table(response2.results))
+// fetch('Data/Movies.json')
+//     .then(response => response.json())
+//     .then(response2 => console.table(response2.results))
 
-const map1 = new Map();
 
-console.log(map1.get(title))
+  fetch("Data/Movies.json")
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    let movieTitles = []
+    for(let key in data) {
+      movieTitles.push(data[key].original_title);
+    }
+    console.log(movieTitles);
+  })
+  .catch(error => console.log(error));
